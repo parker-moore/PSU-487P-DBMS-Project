37559cc0ef68ce8bf1717fffe6c32f66b1741f9a
thewoodencyborg <parkermoore1@frontier.com>
2019-04-13 18:48:42 -0700

    Project Update 4/13/2019
    -added string code
    -added code to create csv file
    -uploaded test csv file w/ 10 data items per list (test file needs to be transposed as the rows and columns are currently switched)

--- generate_data.py
+++ generate_data.py
@@ -6,14 +6,14 @@ def buildUnique(maxTuples, rand):
     list = [i for i in range(0,maxTuples)]
     if rand:
         random.shuffle(list)
-    print(list)
+    #print(list)
     return list
 
 def buildModlist(unique1, modValue):
     list = []
     for i in unique1:
         list.append(i % modValue)
-    print(list)
+    #print(list)
     return list
 
 def buildPercent(onePercent, even):
@@ -23,8 +23,41 @@ def buildPercent(onePercent, even):
             list.append(i * 2)
         else:
             list.append((i * 2) + 1)
-    print(list)
+    #print(list)
     return list
+
+def buildString(size, rand):
+    alpha = [i for i in range(ord('A'),ord('W'))]
+    list = []
+    c = 0
+    
+    for i in alpha:
+        for j in alpha:
+            for k in alpha:
+                if c < size:
+                    list.append(str(chr(k)) + "x"*25 + str(chr(j)) + "x"*24 + str(chr(i)))
+                    c += 1
+                else:
+                    break
+                
+    if(rand):
+        random.shuffle(list)
+    #print(list)
+    return list
+
+def buildString4(size):
+    list = []
+    list.append("A" + "x"*25 + "A" + "x"*24 + "A")
+    list.append("H" + "x"*25 + "H" + "x"*24 + "H")
+    list.append("O" + "x"*25 + "O" + "x"*24 + "O")
+    list.append("V" + "x"*25 + "V" + "x"*24 + "V")
+    c = 0
+    string4 = []
+    while c < size:
+        string4.append(list[c%4])
+        c += 1
+    print (string4)
+    return string4
     
 def buildLists(size):
     unique1 = buildUnique(size, True)
@@ -40,3 +73,32 @@ def buildLists(size):
     unique3 = unique1.copy()
     evenOnePercent = buildPercent(onePercent, True)
     oddOnePercent = buildPercent(onePercent, False)
+    stringu1 = buildString(size, True)
+    stringu2 = buildString(size, False)
+    string4 = buildString4(size)
+    
+    with open('test.csv',mode='w',newline='') as testFile:
+        wr = csv.writer(testFile,quoting=csv.QUOTE_ALL)
+        wr.writerow(unique1)
+        wr.writerow(unique2)
+        wr.writerow(two)
+        wr.writerow(four)
+        wr.writerow(ten)
+        wr.writerow(twenty)
+        wr.writerow(onePercent)
+        wr.writerow(tenPercent)
+        wr.writerow(twentyPercent)
+        wr.writerow(fiftyPercent)
+        wr.writerow(unique3)
+        wr.writerow(evenOnePercent)
+        wr.writerow(oddOnePercent)
+        wr.writerow(stringu1)
+        wr.writerow(stringu2)
+        wr.writerow(string4)
+
+
+def main():
+    buildLists(10)
+
+if __name__ == '__main__':
+    main()
